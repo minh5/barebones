02f5826f65310d27c82af48be458846982e55225
minh5 <minh.v.mai@gmail.com>
2017-04-29 18:23:07 -0400

    added test for perceptron

--- perceptron/models.py
+++ perceptron/models.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Perceptron:
 
@@ -16,18 +17,19 @@ class Perceptron:
         activation = self.bias
         for i in range(len(x)):
             activation += weights[i] * x[i]
-        return 1 if self.threshold >= 0 else 0
+        return 1 if activation >= self.threshold else 0
 
     def run(self):
-        weights = [0 for i in range(len(self.x_train))]
+        weights = np.zeros(self.x_train.shape[1])
         for epoch in range(self.epoch):
             sum_error = 0
-        for x, y in zip(self.x_train, self.y_train):
-            prediction = self.predict(x, weights)
-            error = y - prediction
-            sum_error += error**2
-            self.bias = self.bias + self.learning_rate * error
-        for i in range(len(x)):
-            weights[i] = weights[i] + self.learning_rate * error * x[i]
-            print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, self.learning_rate, sum_error))
+            for x, y in zip(self.x_train, self.y_train):
+                prediction = self.predict(x, weights)
+                error = y - prediction
+                sum_error += error**2
+                self.bias = self.bias + self.learning_rate * error
+                for i in range(len(x)):
+                    weights[i] = weights[i] + self.learning_rate * error * x[i]
+                    print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, self.learning_rate, sum_error))
+        print('weight:', weights)
         return weights
